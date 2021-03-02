e7856a677f6adafb69e21b0d735048a007fc24a3
Mr.Wang hao <59255587+wanghao221@users.noreply.github.com>
2021-03-02 21:42:18 +0800

    Create style.css

---
+++ game/yuansuzhouqibiao/css/style.css
@@ -0,0 +1,49 @@
+@import url("https://fonts.googleapis.com/css2?family=Merriweather:wght@700&display=swap");
+body {
+  margin: 0;
+  justify-content: center;
+  display: flex;
+  background: #222831;
+}
+
+#container {
+  width: 95vw;
+  height: 55vw;
+  background: #262c35;
+  margin-top: 5vw;
+  position: relative;
+  border-radius: 5px;
+  margin-bottom: 5vw;
+}
+
+.element-name {
+  position: absolute;
+  left: 50%;
+  top: 40%;
+  transform: translate(-50%, -50%);
+  font-size: 1.5vw;
+  font-family: "Merriweather", serif;
+  font-weight: 700;
+}
+.element-number {
+  position: absolute;
+  left: 50%;
+  top: 15%;
+  transform: translate(-50%, -50%);
+  font-size: 0.6vw;
+}
+
+.legend {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 0.8vw;
+  font-family: "Merriweather", serif;
+  font-weight: 700;
+}
+
+.svg {
+  width: 100%;
+  height: 100%;
+}
